cacce1eead0e2a555e6a0614ff03bfb2e3c2521f
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-12 18:46:57 -0400

    phase-1-array-map-method-lab/
    phase-1-array-map-method-lab/

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const result = titleCased();
 console.log(result);
 
 function titleCased() {
-  const capitalizedTitles = tutorials.map(title => {
+  const capitalizedTitles = (tutorials).map(title => {
     const words = title.split(" ");
     const capitalizedWords = words.map(word => {
       return word.charAt(0).toUpperCase() + word.slice(1);
@@ -16,23 +16,20 @@ function titleCased() {
 
 //// Lab: Using map() to Generate a New ArrayLet's put our newly acquired knowledge of map() to use! We just uploaded 10 coding tutorials online, but some of them have inconsistent casing. We want all the titles to be "title case", in other words, the first letter of each word should be capitalized. Create a new array containing the names of the tutorials with proper title case formatting. For example, 'what does the this keyword mean?' should become 'What Does The This Keyword Mean?'.
 //our job is to write the following function:
-
 //titleCased(): returns an array with title case tutorial names. Note that this function takes no arguments and should use the global tutorials variable as data.
 //NOTE: This lab is challenging! You will need to iterate through the tutorials array, modifying the name of each tutorial. To do this, you will also need to access and modify each individual word.
-
 ////Some questions to consider:
-
 //How can we "iterate" through individual words in a string?
 //Can we execute an iteration inside an iteration? How?
 //How can we capitalize just the first letter in a word?
 //A couple of hints:
-
 //Break the task into smaller chunks: using the console or a REPL, start by figuring out how to modify one individual element in the tutorials array. Once you've got that working, then figure out how to update the array itself.
 //Use Google!!
 //Remember the workflow:
-
 //Install the dependencies using npm install.
 /////Run the tests using npm test.
 //Read the errors; vocalize what they're asking you to do. //Pseudocode
+
+
 
 
